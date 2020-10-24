e0795b1add7f311fe82bc71407b68fc05e26ec73
Abdu Jemal <abduh9350@gmail.com>
2020-10-24 04:51:43 -0400

    airbnb clone project started

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import LanguageIcon from '@material-ui/icons/Language'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { Avatar } from "@material-ui/core";
+function Header() {
+    return (
+        <div className='header'>
+            <img 
+                className='header_icon'
+                src='https://static.dezeen.com/uploads/2014/07/Airbnb-rebrand-by-DesignStudio_dezeen_468_8-300x300.jpg'
+                alt=''
+            />
+            <div className="header_center">
+                <input type='text' />
+                <SearchIcon />
+            </div>
+            <div className="header_right">
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
+export default Header
