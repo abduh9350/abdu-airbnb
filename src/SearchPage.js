7f8e057c59797e8544966f30300342278f3760cc
Abdu Jemal <abduh9350@gmail.com>
2020-11-07 01:59:24 -0500

    card css file edited

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,48 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import './SearchPage.css'
+import SearchResult from './SearchResult'
 function SearchPage() {
     return (
         <div className='searchPage'>
-            <h1>This is Search Page</h1>
+            <div className="searchPage_info">
+                <p>62 stays . 26 august to 30 august . 2 guest</p>
+                <h1>Stays nearby</h1>
+                <Button variant='outlined'>
+                    Cancellation Flexibility
+                </Button>
+                <Button variant='outlined'>
+                    Type of place
+                </Button>
+                <Button variant='outlined'>
+                    Price
+                </Button>
+                <Button variant='outlined'>
+                    Rooms and beds
+                </Button>
+                <Button variant='outlined'>
+                    More filters
+                </Button>
+
+            </div>
+            <SearchResult 
+                img='https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&w=1000&q=80'
+                location='Private Room in center of London'
+                title='Villa Oak'
+                description='1 guest . 1 bed . 3/2 shared bathrooms . WIFI . Kitchen . Free parking . Washing Machine '
+                star={4.73}
+                price='$200/night'
+                total='$117 total'
+            />
+            <SearchResult 
+                img='https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&w=1000&q=80'
+                location='Private Room in center of London'
+                title='Villa Oak'
+                description='1 guest . 1 bed . 3/2 shared bathrooms . WIFI . Kitchen . Free parking . Washing Machine '
+                star={4.73}
+                price='$200/night'
+                total='$117 total'
+            />
         </div>
     )
 }
---
+++ src/SearchResult.js
@@ -0,0 +1,34 @@
+import { FavoriteBorder, Star } from '@material-ui/icons'
+import React from 'react'
+import './SearchResult.css'
+
+function SearchResult({ img, location, title, description, star, price, total}) {
+    return (
+        <div className='searchResult'>
+            <img src={img} alt='' />
+            <FavoriteBorder 
+                className='searchResult_heart'
+            />
+            <div className="searchResult_info">
+                <div className="searchResult_infoTop">
+                    <p>{location}</p>
+                    <h3>{title}</h3>
+                    <p>____</p>
+                    <p>{description}</p>
+                </div>
+                <div className="searchResult_infoBottom">
+                    <div className="searchResult_stars">
+                        <Star className='searchResult_star' />
+                        <p><strong>{star}</strong></p>
+                    </div>
+                    <div className="searchResult_price">
+                       <h2>{price}</h2>
+                       <p>{total}</p> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchResult
